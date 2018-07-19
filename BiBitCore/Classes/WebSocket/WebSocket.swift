54a5789828b15b95b5673f5672e2945f36f67b76
Podkorytov iEvgen <app.podkorytov@gmail.com>
2018-07-19 15:17:40 +0300

    Update WebSocket.swift

--- BiBitCore/Classes/WebSocket/WebSocket.swift
+++ BiBitCore/Classes/WebSocket/WebSocket.swift
@@ -4,13 +4,6 @@
 //
 /*
  * SwiftWebSocket (websocket.swift)
- *
- * Copyright (C) Josh Baker. All Rights Reserved.
- * Contact: @tidwall, joshbaker77@gmail.com
- *
- * This software may be modified and distributed under the terms
- * of the MIT license.  See the LICENSE file for details.
- *
  */
 
 import Foundation
